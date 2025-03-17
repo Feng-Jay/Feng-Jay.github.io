515d670d6af26447c0cfb4cfe245185ff289ce9c
ffengjay <2652925120@qq.com>
2025-03-17 11:10:58 +0800

    Quartz sync: Mar 17, 2025, 11:10 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,7 +6,21 @@ import { SimpleSlug } from "./quartz/util/path"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'Feng-Jay/Feng-Jay.github.io',
+        // from data-repo-id
+        repoId: 'R_kgDOH8cFTw',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOH8cFT84CoHp_',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
